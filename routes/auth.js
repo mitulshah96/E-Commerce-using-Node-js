5a9963ef31dfb234dfa9e9804a71af0d2ab609b9
mitul shah <mitul.shah@accionlabs.com>
2019-08-06 19:53:27 +0530

    added validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check, body } = require('express-validator/check');
+const User = require('../models/user');
 
 const authController = require('../controllers/auth');
 
@@ -10,7 +12,36 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+    '/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email')
+            .custom((value, { req }) => {
+                return User.findOne({ email: value }).then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject(
+                            'E-Mail exists already, please pick a different one.'
+                        );
+                    }
+                });
+            }),
+        body(
+            'password',
+            'please enter a password with only numbers and text and at least 5 characters'
+        )
+            .isLength({ min: 5 })
+            .isAlphanumeric(),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Password doesnt match');
+            }
+            return true;
+        })
+    ],
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
